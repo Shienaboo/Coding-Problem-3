84eedd4b119c2e27d353f1229fd17259dba91de2
unknown <nshienarocell@gmail.com>
2024-11-04 18:36:24 +0800

    initial commit

---
+++ userAge.js
@@ -0,0 +1,15 @@
+let age = "20";
+
+if (age < 3) {
+    console.log("You are a toddler.");
+} else if (age <= 12) {
+    console.log("You are a child.");
+} else if (age <= 18) {
+    console.log("You are a teenager.");
+} else if (age <= 25) {
+    console.log("You are a young adult.");
+} else if (age <= 65) {
+    console.log("You are middle-aged.");
+} else {
+    console.log("You are a senior.")
+}
